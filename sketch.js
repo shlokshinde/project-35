86235b22a7c5263daaf13933291071c1afe0fedf
shlokshinde <75329415+shlokshinde@users.noreply.github.com>
2021-04-18 14:08:06 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -32,22 +32,22 @@ function draw() {
   if(keyDown(LEFT_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
     //write code to move air balloon in left direction
-    writePosition(-1,0);
+    writePosition(-5,0);
   }
   else if(keyDown(RIGHT_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
     //write code to move air balloon in right direction
-    writePosition(1,0);
+    writePosition(5,0);
   }
   else if(keyDown(UP_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
     //write code to move air balloon in up direction
-    writePosition(0,-1);
+    writePosition(0,-3);
   }
   else if(keyDown(DOWN_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
     //write code to move air balloon in down direction
-    writePosition(0,1);
+    writePosition(0,3);
   }
   
 
